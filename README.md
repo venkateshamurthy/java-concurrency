42b77ac0cb40d3cb57828d0a42fe91455f790b24
venkat murthy <ts_v_murthy@yahoo.com>
2015-09-08 14:45:16 +0530

    Added test code additionally. added some read me as well.

--- README.md
+++ README.md
@@ -2,3 +2,98 @@
 
 This project is all about my own experiments to understand java concurrency utilities.
 Some libraries used of interest are lombok.
+
+Package concurrent.util.contextual
+==================================
+This package has the following flavours to deal with passing a context associated with Runnable to a Thread in thread pool.
+
+ContextalThreadPoolExecutor
+---------------------------
+It has other associated classes to wrap thread, runnable with contextual flavour. this basically sets a ContextualRunnable's context to a thread in the beforeExecute method.
+
+A sample test use:
+
+```java
+    
+    private static final String context = "User:murthy";
+    @Test
+    public void test() {
+        ContextualThreadPoolExecutor<String> ctp= ContextualThreadPoolExecutor.newFixedThreadPool(10);
+        Runnable runnable = new Runnable(){
+            @Override
+            public void run() {
+                ContextualThread<String> ct;
+                if((ct=ContextualThread.current()) !=null){
+                    log.info("From {}.run method,expected-context:{} got-context:{}",ct.getName(),newContext,ct.getContext());
+                    Assert.assertEquals("Context "+ct.getContext(),newContext, ct.getContext());    
+                }
+            }
+        };
+        //ctp.submit(runnable);
+        ContextualRunnable<String> ctxRunnable = ContextualRunnable.make(newContext,runnable);
+        ctp.submit(ctxRunnable);
+        
+        try {
+            TimeUnit.SECONDS.sleep(2);
+            ctp.shutdown();
+            ctp.awaitTermination(-1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Assert.assertNull(ctxRunnable.getThrowable());
+    }
+```
+MDCCallable and MDCRunnable
+---------------------------
+This assumes MDC from Slf4j as the means to capture per-thread specific context. Akin to memento; the context at the time of creation of runnable/callable will be set during execution by the thread and unset before the run/call finishes.
+
+A sample test method reproduced from src/test/java.
+
+```java
+
+    @Slf4j
+    public class MDCExecutorTest {
+    ExecutorService es;
+    Map<String, String> context = Collections.singletonMap("User", "Murthy");
+    @Test
+    public void testRunnable() {
+        final CyclicBarrier cb = new CyclicBarrier(2); // since only one more
+                                                        // thread apart from
+                                                        // main
+        Runnable runner = new Runnable() {
+            @Override
+            public void run() {
+                log.info(MDC.getCopyOfContextMap().toString());
+                try {
+                    cb.await();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Assert.assertTrue(context.equals(MDC.getCopyOfContextMap()));
+            }
+        };
+        MDCRunnable mdcRunner = MDCRunnable.of(context, runner);
+        es.submit(mdcRunner);
+        try {
+            cb.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Assert.assertNull(mdcRunner.getThrowable());
+    }
+    @Before
+    public void before() {
+        es = Executors.newFixedThreadPool(10);
+    }
+
+    @After
+    public void after() {
+        try {
+            es.shutdown();
+            es.awaitTermination(-1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+```
--- src/test/java/concurrent/util/contextual/MDCExecutorTest.java
+++ src/test/java/concurrent/util/contextual/MDCExecutorTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.fail;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,68 +15,135 @@ import java.util.concurrent.TimeUnit;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatchman;
+import org.junit.runners.model.FrameworkMethod;
 import org.slf4j.MDC;
 
 @Slf4j
 public class MDCExecutorTest {
 
 	Map<String, String> context = Collections.singletonMap("User", "Murthy");
+	Map<String, String> newContext = Collections.singletonMap("User", "Lalit");
+	ExecutorService es;
+
+	  @Rule
+	  public TestWatchman watchman= new TestWatchman() {
+			/**
+			 * Invoked when a test method fails
+			 * 
+			 * @param e 
+			 * @param method
+			 */
+			public void failed(Throwable e, FrameworkMethod method) {
+				log.debug("{} : FAILED",method.getName());
+			}
+
+			/**
+			 * Invoked when a test method is about to start
+			 * 
+			 * @param method  
+			 */
+			public void starting(FrameworkMethod method) {
+				log.debug("{} : STARTING",method.getName());
+			}
+
+
+			/**
+			 * Invoked when a test method finishes (whether passing or failing)
+			 * 
+			 * @param method  
+			 */
+			public void finished(FrameworkMethod method) {
+				log.debug("{} : FINISHED",method.getName());
+			}
+	     };
+ 
+
+	@Before
+	public void before() {
+		es = Executors.newFixedThreadPool(10);
+	}
+
+	@After
+	public void after() {
+		try {
+			es.shutdown();
+			es.awaitTermination(-1, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test(expected = AssertionError.class)
 	public void testRunnableForAssertionError() {
+		final CyclicBarrier cb = new CyclicBarrier(2); // since only one more
+														// thread apart from
+														// main
 		Runnable runner = new Runnable() {
 			@Override
 			public void run() {
 				log.info(MDC.getCopyOfContextMap().toString());
-				Assert.assertFalse(context.equals(MDC.getCopyOfContextMap()));//deliberately throwing
+				try {
+					cb.await();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				Assert.assertFalse(context.equals(MDC.getCopyOfContextMap()));// deliberately
+																				// throwing
 			}
 		};
 		MDCRunnable mdcRunner = MDCRunnable.of(context, runner);
-
-		ExecutorService es = Executors.newFixedThreadPool(2);
 		es.submit(mdcRunner);
 		try {
-			TimeUnit.SECONDS.sleep(1);
-			es.shutdown();
-			es.awaitTermination(-1, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
+			cb.await();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Assert.assertNotNull(mdcRunner.getThrowable());
+	}
+
+	@Test
+	public void testRunnable() {
+		final CyclicBarrier cb = new CyclicBarrier(2); // since only one more
+														// thread apart from
+														// main
+		Runnable runner = new Runnable() {
+			@Override
+			public void run() {
+				log.info(MDC.getCopyOfContextMap().toString());
+				try {
+					cb.await();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				Assert.assertTrue(context.equals(MDC.getCopyOfContextMap()));
+			}
+		};
+		MDCRunnable mdcRunner = MDCRunnable.of(context, runner);
+		es.submit(mdcRunner);
+		try {
+			cb.await();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		Assert.assertNull(mdcRunner.getThrowable());
 	}
 
 	@Test
-	public void testRunnable() {
-		Runnable runner = new Runnable() {
-			@Override
-			public void run() {
-				log.info(MDC.getCopyOfContextMap().toString());
-				Assert.assertTrue(context.equals(MDC.getCopyOfContextMap()));
-			}
-		};
-		MDCRunnable mdcRunner = MDCRunnable.of(context, runner);
-
-		ExecutorService es = Executors.newFixedThreadPool(2);
-		es.submit(mdcRunner);
-		try {
-			TimeUnit.SECONDS.sleep(1);
-			es.shutdown();
-			es.awaitTermination(-1, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		Assert.assertNull(mdcRunner.getThrowable());//make sure that there is no throwable set
-	}
-
-	@Test
 	public void testCallableForAssertionError() {
+		// final CyclicBarrier cb = new CyclicBarrier(2); // since only one more
+		// thread apart from main
 		Callable<Map<String, String>> caller = new Callable<Map<String, String>>() {
 			@Override
 			public Map<String, String> call() throws Exception {
 				Map<String, String> map = MDC.getCopyOfContextMap();
 				log.info(map.toString());
+				// try {cb.await();} catch (Exception e) { e.printStackTrace();}
 				Assert.assertFalse(context.equals(map));
 				return map;
 			}
@@ -82,23 +151,15 @@ public class MDCExecutorTest {
 
 		MDCCallable<Map<String, String>> mdcCaller = MDCCallable.of(context,
 				caller);
-
-		ExecutorService es = Executors.newFixedThreadPool(2);
-
+		Future<Map<String, String>> future = es.submit(mdcCaller);
 		try {
-			Future<Map<String, String>> future = es.submit(mdcCaller);
-			fail();
-		} catch (AssertionError e) {
-			// Dont do anything..its expected
-		}
-
-		try {
-			TimeUnit.SECONDS.sleep(1);
-			es.shutdown();
-			es.awaitTermination(-1, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
+			future.get();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		// try {cb.await();} catch (Exception e) { e.printStackTrace();}
+		// after();
 		Assert.assertNotNull(mdcCaller.getThrowable());
 	}
 
@@ -115,18 +176,68 @@ public class MDCExecutorTest {
 		};
 		MDCCallable<Map<String, String>> mdcCaller = MDCCallable.of(context,
 				callaer);
-
-		ExecutorService es = Executors.newFixedThreadPool(2);
 		Future<Map<String, String>> future = es.submit(mdcCaller);
 		try {
 			Assert.assertEquals(context, future.get());
-			TimeUnit.SECONDS.sleep(1);
-			es.shutdown();
-			es.awaitTermination(-1, TimeUnit.SECONDS);
 		} catch (InterruptedException | ExecutionException e1) {
 			e1.printStackTrace();
 			fail();
 		}
 		Assert.assertNull(mdcCaller.getThrowable());
+	}
+
+	@Test
+	public void testThreadSpecificMDC() {
+		final CyclicBarrier barrier = new CyclicBarrier(3);
+		// context.put("User", "Venkat");
+		Runnable runnerWithContext = new Runnable() {
+			@Override
+			public void run() {
+				log.info("testThreadSpecificMDC.runnerWithContext:{}", MDC
+						.getCopyOfContextMap().toString());
+				Assert.assertTrue(context.equals(MDC.getCopyOfContextMap()));
+				try {
+					barrier.await();
+				} catch (Exception e) {
+				}
+			}
+		};
+
+		Runnable runnerWithNewContext = new Runnable() {
+			@Override
+			public void run() {
+				log.info("testThreadSpecificMDC.runnerWithNewContext:{}", MDC
+						.getCopyOfContextMap().toString());
+				Assert.assertTrue(newContext.equals(MDC.getCopyOfContextMap()));
+				try {
+					barrier.await();
+				} catch (Exception e) {
+				}
+			}
+		};
+
+		MDCRunnable mdcRunnerContext = MDCRunnable.of(context,
+				runnerWithContext);
+		MDCRunnable mdcRunnerNewContext = MDCRunnable.of(newContext,
+				runnerWithNewContext);
+
+		es.submit(mdcRunnerContext);
+		es.submit(mdcRunnerNewContext);
+		try {
+			barrier.await();
+			log.info(
+					"testThreadSpecificMDC.All runnables under barrier finished : {}",
+					barrier.getNumberWaiting() == 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("Error:", e);
+		}
+		Assert.assertNull(mdcRunnerContext.getThrowable());// make sure that
+															// there is no
+															// throwable set
+		Assert.assertNull(mdcRunnerNewContext.getThrowable());// make sure that
+																// there is no
+																// throwable set
+
 	}
 }
